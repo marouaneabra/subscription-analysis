17847b0cdb94b1f45525f68a7bed13bf96133c85
Marouane <marouane.abra@gmail.com>
2018-10-20 15:09:34 -0400

    changing html with js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-<script>
-document.getElementById("getStarted").addEventListener("click", getFirstQ);
-
+//document.getElementById("getStarted").addEventListener("click", getFirstQ);
+$("#getStarted").on('click', getFirstQ);
 function getFirstQ() {
     document.getElementById("getStarted").innerHTML = Date();
+    document.write("NOOOOOO");
 }
-</script>
