d8f4abeec03b563050461d641d7ec2a64d78231a
Marouane <marouane.abra@gmail.com>
2018-10-20 16:32:09 -0400

    added input page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 //document.getElementById("getStarted").addEventListener("click", getFirstQ);
+
 $("#getStarted").on('click', getFirstQ);
+
+
 function getFirstQ() {
-    document.getElementById("getStarted").innerHTML = Date();
-    document.write("NOOOOOO");
+    //document.getElementById("getStarted").innerHTML = Date();
+    window.location.href = "input.html";
 }
